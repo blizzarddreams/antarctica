bdbb0884f21d0b2936f24ac9686df2631056ed1d
notblizzard <arcaninebellies@gmail.com>
2023-08-31 19:20:07 -0400

    Add ability to post from dashboard

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -5,7 +5,8 @@ import { useCallback, useEffect, useState } from "react";
 import Post from "../utils/Post";
 import InfiniteScroll from "react-infinite-scroll-component";
 import { PusherClient } from "@/pusher";
-
+import Image from "next/image";
+import { CameraIcon } from "@heroicons/react/24/outline";
 interface User {
   id: number;
   email: string;
@@ -44,6 +45,28 @@ export default function Dashboard() {
   const [posts, setPosts] = useState<Post[]>([]!);
   const [skip, setSkip] = useState(0);
   const [hasMore, setHasMore] = useState<boolean>(true);
+  const [image, setImage] = useState("");
+  const [post, setPost] = useState("");
+  const [preview, setPreview] = useState("");
+
+  const handleImageChange = (e) => {
+    setPreview(URL.createObjectURL(e.target.files[0]));
+    setImage(e.target.files[0]);
+  };
+
+  const makePost = async () => {
+    const form = new FormData();
+    if (image) form.append("image", image);
+    form.append("post", post);
+    fetch("/api/post", {
+      method: "POST",
+      body: form,
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        setPost("");
+      });
+  };
 
   const getData = useCallback(() => {
     if (session) {
@@ -65,9 +88,7 @@ export default function Dashboard() {
     if (session?.user?.email) {
       const channel = PusherClient.subscribe(`dashboard-${session.user.email}`);
       channel.bind("new message", (data) => {
-        const posts_ = posts;
-        posts_.unshift(data.post);
-        setPosts(posts_);
+        setPosts([...[data.post].concat(posts)]);
       });
     }
   }, [session]);
@@ -75,6 +96,57 @@ export default function Dashboard() {
     <>
       {posts && (
         <>
+          <div className="mt-2 w-full">
+            <textarea
+              id="content"
+              name="content"
+              rows={4}
+              value={post}
+              onChange={(e) => setPost(e.target.value)}
+              placeholder="What is happening?"
+              className="resize-none w-full border-none focus:border-none bg-slate-800 text-white outline-none focus:outline-none focus:ring-0"
+            />
+          </div>
+
+          <div className="mt-4 flex flex-row w-full items-center">
+            {post.replaceAll(" ", "").length === 0 ? (
+              <button
+                type="button"
+                className=" bg-slate-800/70 p-4 my-1 w-full mr-4"
+                onClick={makePost}
+                disabled
+              >
+                Post
+              </button>
+            ) : (
+              <button
+                type="button"
+                className=" bg-slate-800 p-4 my-1 w-full mr-4"
+                onClick={makePost}
+              >
+                Post
+              </button>
+            )}
+            {280 - post.length > 20 ? (
+              <p className="text-teal-400"> {280 - post.length}</p>
+            ) : (
+              <p className="text-rose-400"> {280 - post.length}</p>
+            )}
+            <label htmlFor="image">
+              <CameraIcon className="h-10 w-10" />
+            </label>
+            <input
+              type="file"
+              name="image"
+              id="image"
+              accept="image/png, image/jpeg, image/jpg"
+              onChange={handleImageChange}
+              className="hidden file:bg-gray-800 file:text-white file:border file:border-none block w-full border border-gray-700 cursor-pointer bg-gray-50 dark:bg-gray-700"
+            />
+            {preview && (
+              <Image src={preview} alt={"lol"} height={100} width={100} />
+            )}
+          </div>
           <InfiniteScroll
             dataLength={posts.length}
             next={getData}
