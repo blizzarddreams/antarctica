0d1fcf60e7c66d3ec477f2fad82cab37c3816d12
Blizzard <arcaninebellies@gmail.com>
2023-09-03 05:30:45 +0000

    Add Discord as OAuth option

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,15 +1,21 @@
 import { PrismaClient } from "@prisma/client";
 import NextAuth from "next-auth";
-import GithubHandler from "next-auth/providers/github";
+import GithubProvider from "next-auth/providers/github";
+import DiscordProvider from "next-auth/providers/discord";
 import prisma from "@/prisma";
 
 export const OPTIONS = {
   providers: [
-    GithubHandler({
+    GithubProvider({
+      clientId: process.env.GITHUB_CLIENT_ID!,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET!,
+    }),
+    DiscordProvider({
       clientId: process.env.GITHUB_CLIENT_ID!,
       clientSecret: process.env.GITHUB_CLIENT_SECRET!,
     }),
   ],
+
   callbacks: {
     async signIn({ user, account, profile, email, credentials }) {
       const user_ = await prisma.user.findFirst({
