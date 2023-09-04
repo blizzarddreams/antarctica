d0a4b18a5adc418cfb75b88faec7d618fabb37ed
notblizzard <arcaninebellies@gmail.com>
2023-09-04 15:39:51 -0400

    Remove tests from app.spec.ts

--- tests/app.spec.ts
+++ tests/app.spec.ts
@@ -5,18 +5,3 @@ test("has title", async ({ page }) => {
 
   await expect(page).toHaveTitle(/antarctica/);
 });
-
-test("has explore link", async ({ page }) => {
-  await page.goto("http://localhost:7040");
-
-  await expect(page.getByRole("button", { name: /Explore/i })).toHaveText(
-    "Explore",
-  );
-});
-
-test("has explore header", async ({ page }) => {
-  await page.goto("http://localhost:7040");
-  await page.getByRole("button", { name: /Explore/i }).click();
-  await page.waitForURL("**/explore");
-  await expect(page.getByText("Explore")).toBeVisible();
-});
