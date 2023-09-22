8ae62c4140ae10c4e9203872b74fcc316e2c1486
notblizzard <arcaninebellies@gmail.com>
2023-09-22 15:42:19 -0400

    Updates

--- upload.ts
+++ upload.ts
@@ -1,13 +1,16 @@
 import { v4 as uuidv4 } from "uuid";
-import { put } from "@vercel/blob";
+import { s3Client } from "./spaces";
+import { PutObjectCommand } from "@aws-sdk/client-s3";
 
 export default async function upload(image: Buffer, folder: string) {
   const uuid = uuidv4();
+  let bucketParams = {
+    Bucket: "arcanines",
+    Key: `antarctica/${folder}/${uuid}.png`,
+    Body: image,
+    ACL: "public-read",
+  };
+  const data = await s3Client.send(new PutObjectCommand(bucketParams));
 
-  const blob = await put(`${folder}/${uuid}.png`, image, {
-    contentType: "image/png",
-    access: "public",
-  });
-
-  return blob.url;
+  return uuid;
 }
