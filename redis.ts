8681731a8e97bf60e535069e3a309d81d4e9f4cc
notblizzard <arcaninebellies@gmail.com>
2023-09-21 21:42:49 -0400

    Update redis

--- redis.ts
+++ redis.ts
@@ -1,6 +1,8 @@
 import { createClient } from "redis";
 
-const redis = createClient();
+const redis = createClient({
+  url: process.env.KV_URL || process.env.REDIS_URL,
+});
 
 (async () => {
   await redis.connect();
